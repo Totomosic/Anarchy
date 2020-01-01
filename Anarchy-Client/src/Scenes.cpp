f5bf46525e91d3c41eac3a98026ea5e3492bb5f0
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2020-01-01 20:57:03 +0800

    Updated ui for new api

--- Anarchy-Client/src/Scenes.cpp
+++ Anarchy-Client/src/Scenes.cpp
@@ -17,16 +17,16 @@ namespace Anarchy
 		ResourcePtr<Font> titleFont = ResourceManager::Get().Fonts().Calibri(36);
 		ResourcePtr<Font> displayFont = ResourceManager::Get().Fonts().Calibri(18);
 
-		UIFactory ui = layer.GetUI().GetRoot().GetFactory();
+		UIElement& ui = layer.GetUI().GetRoot();
 		UIRectangle& background = ui.CreateRectangle(500, 600, Color::White, Transform({ window.Width() / 2.0f, window.Height() / 2.0f, 0 }));
-		background.GetFactory().CreateText("Anarchy", titleFont, Color::Black, Transform({ 0, 125, 1 }));
+		background.CreateText("Anarchy", titleFont, Color::Black, Transform({ 0, 125, 1 }));
 
-		UITextInput& serverInput = background.GetFactory().CreateTextInput(300, 50, displayFont, Color::Black, Color(128, 128, 128), Transform({ 0, 0, 1 }));
-		serverInput.GetFactory().CreateText("Server:", displayFont, Color::Black, Transform({ -150, 30, 1 }), AlignH::Left, AlignV::Bottom);
+		UITextInput& serverInput = background.CreateTextInput(300, 50, displayFont, Color::Black, Color(128, 128, 128), Transform({ 0, 0, 1 }));
+		serverInput.CreateText("Server:", displayFont, Color::Black, Transform({ -150, 30, 1 }), AlignH::Left, AlignV::Bottom);
 		serverInput.SetText("localhost:10000");
 
-		UIRectangle& connectButton = background.GetFactory().CreateRectangle(300, 50, Color(50, 200, 50), Transform({ 0, -60, 1 }));
-		connectButton.GetFactory().CreateText("Connect", displayFont, Color::Black, Transform({ 0, 0, 1 }));
+		UIRectangle& connectButton = background.CreateRectangle(300, 50, Color(50, 200, 50), Transform({ 0, -60, 1 }));
+		connectButton.CreateText("Connect", displayFont, Color::Black, Transform({ 0, 0, 1 }));
 		connectButton.Events().OnClick().AddEventListener([&serverInput, &gameScene](Event<UI<MouseClickEvent>>& e)
 			{
 				ConnectToServerEvent data;
@@ -58,7 +58,7 @@ namespace Anarchy
 					});
 			});
 
-		background.GetFactory().CreateText("Connecting As: Totomosic", displayFont, Color::Black, Transform({ 0, -100, 1 }));
+		background.CreateText("Connecting As: Totomosic", displayFont, Color::Black, Transform({ 0, -100, 1 }));
 	}
 
 	void CreateGameScene(Scene& scene, const Window& window)
